dae75e1c19a326a853ed0206846181f27223f3a3
gordon <1176784982@qq.com>
2023-02-28 18:02:48 +0800

    good

--- main.py
+++ main.py
@@ -3,15 +3,21 @@
 import pandas as pd
 
 def process(): #处理过程
-    df_AI = pd.read_excel("AIW.xlsx").iloc[:10,0]#读入 AIW.xlsx
+    df_AI = pd.read_excel("AIW.xlsx").iloc[:10]#读入 AIW.xlsx
     df_ATTA = pd.read_excel("ATTA.xlsx")#读入 ATTA.xlsx
-    result = pd.DataFrame()  # 存放结果 最后输出成excel 查看结果是否正确
-    for i in range(0,len(df_AI),1):
+    for i in df_AI.index:
         author = df_AI.iloc[i,0] #选取作者列
         innov = df_AI.iloc[i,1] #选取创新列
         #找出 有作者是author 并且 标题或摘要中有该创新词 的 文章 ↓ case = False 大小写不敏感 na = False 无视空行
-        partResult = df_ATTA[df_ATTA['Author'].str.contains(author) & (df_ATTA['Title'].str.contains(innov) | df_ATTA['Abstract'].str.contains(innov))]
-        result = pd.concat([result,partResult])
-    result.to_excel('Result.xlsx');
+        partResult = df_ATTA[df_ATTA['Authors'].str.contains(author,case = False,na = False) & (df_ATTA['Title'].str.contains(innov,case= False,na=False) | df_ATTA['Abstract'].str.contains(innov,case = False,na = False))]
+        for j in partResult.index:
+            year = partResult['Publication Year'][j]
+            num = df_AI[year][i]
+            if num == "":
+                df_AI[year][i] = 1
+            else:
+                df_AI[year] += 1;
+        print("index : "+i)
     return
+    df_AI.to_excel('Result.xlsx')
 process()#执行
--- preprocess.py
+++ preprocess.py
@@ -2,11 +2,15 @@
 import numpy as np
 import pandas as pd
 
-df_AI = pd.read_excel("AIW.xlsx");
-author = pd.read_excel("AIW.xlsx").iloc[25, 0]  # 第26行 第2列 这是一个str
-innov = pd.read_excel("AIW.xlsx").iloc[25, 1]
-print(author + ";" + innov )
-df_ATTA = pd.read_excel("ATTA.xlsx")
-partResult = df_ATTA[df_ATTA['Authors'].str.contains(author,case = False) & (df_ATTA['Title'].str.contains(innov,case = False) | df_ATTA['Abstract'].str.contains(innov,case = False))]
-print(partResult)
 
+# author = pd.read_excel("AIW.xlsx").iloc[25, 0]  # 第26行 第2列 这是一个str
+# innov = pd.read_excel("AIW.xlsx").iloc[25, 1]
+# print(author + ";" + innov )
+# df_ATTA = pd.read_excel("ATTA.xlsx")
+# partResult = df_ATTA[df_ATTA['Authors'].str.contains(author,case = False) & (df_ATTA['Title'].str.contains(innov,case = False) | df_ATTA['Abstract'].str.contains(innov,case = False))]
+# print(partResult)
+
+df_AI = pd.read_excel("AIW.xlsx").iloc[:10]  # 读入 AIW.xlsx
+print(df_AI)
+
+#df_ATTA = pd.read_excel("ATTA.xlsx")  # 读入 ATTA.xlsx
